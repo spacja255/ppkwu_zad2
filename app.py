e6e4f14a70fb6a9ddfe45abb6b7918516eae4ec8
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-11-03 14:55:56 +0100

    Created check route with fake response

--- app.py
+++ app.py
@@ -6,4 +6,19 @@ app = Flask(__name__)
 def index():
     return "STRING CHECK API"
 
+@app.route("/check/<text>")
+def rev(text):
+    response = {
+        "containsUppercase": False,
+        "containsLowercase": False,
+        "containsDigits":    False,
+        "containsSpecial":   False,
+        "uppercaseCount":    0,
+        "lowercaseCount":    0,
+        "digitsCount":       0,
+        "specialCount":      0
+    }
+    
+    return response
+
 app.run("lan", 8080)
